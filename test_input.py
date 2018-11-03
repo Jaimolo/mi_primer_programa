a6c340f925ac259a4f95230bf3280efaee531bbe
navarrojaime2012@yahoo.es <aSereJe12>
2018-11-03 16:30:14 +0100

    Programa while

--- test_input.py
+++ test_input.py
@@ -1,4 +1,10 @@
 
 
-numero_del_usuario = input("Dime un nnumero: ")
-print(numero_del_usuario)
+number_to_guess = 2
+
+user_number = int(input(" Adivina un número: "))
+
+while number_to_guess != user_number:
+    print("Has fallado")
+    user_number = int(input("Introduce otro número: "))
+print(" Has acertado")
